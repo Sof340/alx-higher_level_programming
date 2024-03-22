4d694d716a24b2a6c009735fcef222f78fea9868
Sof340 <sofianeadrar34@gmail.com>
2024-03-22 10:49:12 -0700

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    weight = 0
+    sum_ = 0
+    result = 0
+    for i in range(len(my_list)):
+        weight += my_list[i][1]
+        sum_ += (my_list[i][0] * my_list[i][1])
+    result = sum_ / weight
+    return result
