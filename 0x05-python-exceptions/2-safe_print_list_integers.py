087e23b7474f90e638a99ff08683a66a35d9f709
Sof340 <sofianeadrar34@gmail.com>
2024-04-02 12:33:05 -0700

    fix for 2.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     try:
         i = count = 0
@@ -9,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         print()
         return count
-    except IndexError:
+    except Exception:
         raise
