bd78bc48553cc6ff7fa5225885cacd36caf7e8ff
Sof340 <sofianeadrar34@gmail.com>
2024-04-12 08:14:14 -0700

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,14 +14,14 @@ def matrix_divided(matrix, div):
     for item in matrix:
         for number in item:
             if not isinstance(number, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+                raise TypeError('''matrix must be a matr
+                        ix (list of lists) of integers/floats''')
     sub_matrix_lengths = [len(sub_matrix) for sub_matrix in matrix]
     if (min(sub_matrix_lengths) != max(sub_matrix_lengths)):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    elif div == 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
     result = [[round(number / div, 2) for number in item] for item in matrix]
     return result
