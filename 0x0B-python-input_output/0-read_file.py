2fef8a642c8b4fa7c996b45a2d90480364acc225
Sof340 <sofianeadrar34@gmail.com>
2024-04-16 16:04:46 -0700

    We offer a truly innovative approach to education:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+A module that prints the content of a file.
+'''
+
+def read_file(filename=""):
+    '''
+    A function that prints the content of a file to the stdout
+    Arguements:
+        filename: the name of the file (the path)
+    '''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end='')
+    f.close()
