069b10920f95b86f37e3a6594b1c118966585d8f
Sof340 <sofianeadrar34@gmail.com>
2024-05-23 16:18:46 -0700

    9. Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT C.id,state_id,C.name,S.name FROM cities C join states S ON C.state_id=S.id;
+SELECT C.id,state_id,C.name,S.name FROM cities C JOIN states S ON C.state_id=S.id;
