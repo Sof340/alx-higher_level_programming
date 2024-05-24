758c88788beefa578d9ffbb656732d3b5ac054ba
Sof340 <sofianeadrar34@gmail.com>
2024-05-23 18:23:48 -0700

    20. Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,2 @@
 -- a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT g.name,SUM(rate) AS rating FROM tv_genres g LEFT JOIN tv_show_genres gr ON g.id=gr.genre_id LEFT join tv_shows s ON gr.show_id=s.id LEFT JOIN tv_show_ratings gs ON s.id=gs.show_id GROUP BY g.name ORDER BY ratin
-g DESC;
+SELECT g.name,SUM(rate) AS rating FROM tv_genres g LEFT JOIN tv_show_genres gr ON g.id=gr.genre_id LEFT JOIN tv_shows s ON gr.show_id=s.id LEFT JOIN tv_show_ratings gs ON s.id=gs.show_id GROUP BY g.name ORDER BY rating DESC;
