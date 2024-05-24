d995abd364cca6e040d272b27582071eeee4fc2e
Sof340 <sofianeadrar34@gmail.com>
2024-05-23 18:07:57 -0700

    19. Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,2 @@
+-- a script that lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT s.title,COUNT(sg.rate) AS rating FROM tv_shows s LEFT JOIN tv_show_ratings sg ON s.id=sg.show_id GROUP BY s.title ORDER BY rating DESC;
