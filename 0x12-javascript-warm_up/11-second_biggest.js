a05540ce6b931edb7aa7b0c2818f054573eac749
Sof340 <sofianeadrar34@gmail.com>
2024-06-18 16:31:42 -0700

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (isNaN(args[0]) || args.length === 1) {
+if (isNaN(parseInt(args[0])) || args.length === 1) {
   console.log(0);
 } else {
-  let first = args[0];
+  let first = parseInt(args[0]);
   let second;
   for (let x = 1; x < args.length; x++) {
-    if (first < args[x]) {
+    if (first < parseInt(args[x])) {
       second = first;
-      first = args[x];
+      first = parseInt(args[x]);
     }
   }
   if (isNaN(second)) {
-    second = args[1];
+    second = parseInt(args[1]);
     for (let x = 2; x < args.length; x++) {
-      if (second < args[x]) {
-        second = args[x];
+      if (second < parseInt(args[x])) {
+        second = parseInt(args[x]);
       }
     }
   }
