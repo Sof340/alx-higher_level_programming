b450bc31adc33912ce65dd27cd00bcf06ce1df21
Sof340 <sofianeadrar34@gmail.com>
2024-06-19 04:24:09 -0700

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (!(h <= 0 || w <= 0 || !w || !h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let mes = '';
+    for (let x = 0; x < this.width; x++) {
+      mes += 'X';
+    }
+
+    for (let x = 0; x < this.height; x++) {
+      console.log(mes);
+    }
+  }
+}
+
+module.exports = Rectangle;
