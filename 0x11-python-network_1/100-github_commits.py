6b3aad9b46472b6b0e7e5a67ec628ea51417f052
Sof340 <sofianeadrar34@gmail.com>
2024-09-26 01:13:27 +0100

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,20 +5,18 @@ this script takes 2 arguments in order to solve
 the challenge.
 """
 
-
 import sys
 import requests
-from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    url = 'https://api.github.com/user'
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
 
-    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    response = requests.get(url)
+    commits = response.json()
 
-    if response.status_code == 200:
-        user_data = response.json()
-        print(user_data['id'])
-    else:
-        print("Failed to retrieve data")
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
